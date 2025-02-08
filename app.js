f5a850bda5c0404f694a955d53fc90bc2abcdc44
xavierb <194048107+xavierb117@users.noreply.github.com>
2025-02-07 21:44:58 -0800

    Added an in-memory database and fixed the css

--- app.js
+++ app.js
@@ -4,12 +4,26 @@ const app = express();
 
 app.use(express.static('public'));
 
+app.use(express.urlencoded({extended:true}));
+
 const PORT = 3000;
+
+const submission = [];
 
 app.get('/', (req, res) => {
     res.sendFile(`${import.meta.dirname}/views/home.html`);
 });
 
+app.post('/submit-guestbook', (req, res) => {
+    submission.push(req.body);
+    console.log(req.body);
+    res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+});
+
+app.get('/admin/orders', (req, res) => {
+    res.send(submission);
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
-})
+});
---
+++ public/script/script.js
@@ -0,0 +1,4 @@
+let button = document.getElementById("switch");
+button.onclick = function() {
+    location.href = "/";
+}
